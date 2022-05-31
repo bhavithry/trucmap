3c51f2fc1535e819c5470823ab441d0f946b06eb
Bhavithry <68148445+bhavithry@users.noreply.github.com>
2022-05-30 17:32:54 -0700

    Update __init__.py

--- trucmap/__init__.py
+++ trucmap/__init__.py
@@ -1,3 +1,3 @@
-from functions import cmap1
-from functions import cmap2
-from functions import cmap_reset
+from myfunctions import cmap1
+from myfunctions import cmap2
+from myfunctions import cmap_reset
